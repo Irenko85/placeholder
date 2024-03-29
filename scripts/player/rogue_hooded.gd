d902dc7141e1d025b930cb418891007d478ddc7a
Cris Isla <cristobal.isla@ug.uchile.cl>
2024-03-29 19:40:07 -0300

    add camera movement

--- scripts/player/rogue_hooded.gd
+++ scripts/player/rogue_hooded.gd
@@ -6,17 +6,27 @@ const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var spring_arm_pivot = $SpringArmPivot
+@export var sensitivity: float = 0.3
 
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED 
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("quit"):
+		get_tree().quit()
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * sensitivity))
+		spring_arm_pivot.rotate_x(deg_to_rad(-event.relative.y * sensitivity))
+		spring_arm_pivot.rotation.x = clamp(spring_arm_pivot.rotation.x, -PI/3, PI/4)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
