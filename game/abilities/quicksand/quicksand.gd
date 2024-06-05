d1c276170ec65ae6ed24a9426a399927450e6b8f
Nicolás I. Peña Henríquez <cs.cubes.2001@gmail.com>
2024-06-05 05:28:46 -0400

    add quicksand scene and files

---
+++ game/abilities/quicksand/quicksand.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+const SLOWNESS: float = 3.0
+const DEFAULT_SPEED: float = 5.0
+
+
+func appear() -> void:
+	position.y = -5
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position:y", -0.9, 1).set_trans(Tween.TRANS_SINE)
+	tween.tween_interval(7.0)
+	tween.tween_callback(disappear)
+
+
+func disappear() -> void:
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "position:y", -5, 5)
+	tween.tween_callback(queue_free)
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	var player = body as Player
+	player.speed /= SLOWNESS
+	player.can_jump = false
+
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	var player = body as Player
+	player.speed = DEFAULT_SPEED
+	player.can_jump = true
+
